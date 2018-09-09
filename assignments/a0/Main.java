70035f5145e89bf7807785693c249804664c031c
a32venka <a32venka@student.cs.uwaterloo.ca>
2018-09-09 14:34:01 -0400

    update main.java file to fix a0 init issues

--- assignments/a0/Main.java
+++ assignments/a0/Main.java
@@ -1,10 +1,26 @@
 import java.io.PrintWriter;
 
-public class Main {
+/*public class Main {
 
     public static void main(String[] args) {
         Runtime.Version version = Runtime.version();
         new Hello(version.toString());
+
+        try {
+            PrintWriter writer = new PrintWriter("results.txt", "UTF-8");
+            writer.println(version);
+            writer.close();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+    }
+}*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        String version = System.getProperty("java.version");
+        new Hello(version);
 
         try {
             PrintWriter writer = new PrintWriter("results.txt", "UTF-8");
