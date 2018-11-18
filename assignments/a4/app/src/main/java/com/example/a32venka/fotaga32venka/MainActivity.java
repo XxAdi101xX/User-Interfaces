dffa251d0490796cb16a107af86517e0ce09802e
Adithya V <adi_101@live.com>
2018-11-17 19:52:43 -0500

    created second activity with link back to the parent activity

--- assignments/a4/app/src/main/java/com/example/a32venka/fotaga32venka/MainActivity.java
+++ assignments/a4/app/src/main/java/com/example/a32venka/fotaga32venka/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a32venka.fotaga32venka;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goNextPage(View view){
+        Intent intent = new Intent(this, PictureActivity.class);
+//        intent.putExtra("version", version);
+        startActivity(intent);
+
     }
 }
