3d3dde06735a34a429500a0c2f19dd875e3488bd
Adithya V <adi_101@live.com>
2018-11-17 21:53:04 -0500

    added reload and clear options onto the toolbar

--- assignments/a4/app/src/main/java/com/example/a32venka/fotaga32venka/MainActivity.java
+++ assignments/a4/app/src/main/java/com/example/a32venka/fotaga32venka/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a32venka.fotaga32venka;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioGroup;
 
@@ -12,6 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_item_clear) {
+
+            // Do something
+            return true;
+        }
+        if (id == R.id.action_item_reload) {
+
+            // Do something
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void goNextPage(View view){
